3c0a085971c47bddd3e9ef78501b1b6c0c9cc7ad
neilm <neil.morrison89@gmail.com>
2020-04-10 12:27:50 +0100

    Class to turn on the console using the controller, to make sure there is a video stream to send over the socket

--- Server_code/GamingStreaming/TurnOnConsole.py
+++ Server_code/GamingStreaming/TurnOnConsole.py
@@ -1,12 +1,25 @@
+import smbus
 import time
-from variables.Configuration import Configuration
 
 
-class TurnOnConsole():
+class TurnOnConsole:
 
     def __init__(self):
-        print("working")
-
+        self.address = 0x04
+        self.bus = smbus.SMBus(1)
 
     def turnOnXbox(self):
-        print("Turn On")
+        self.writeData("P1")
+        time.sleep(0.01)
+        self.writeData("*")
+
+    def writeData(self, value):
+        byteValue = self.StringToBytes(value)
+        self.bus.write_i2c_block_data(self.address, 0x00, byteValue)
+        return -1
+
+    def StringToBytes(self, val):
+        retVal = []
+        for c in val:
+            retVal.append(ord(c))
+        return retVal
