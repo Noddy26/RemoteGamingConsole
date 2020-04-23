f4f7dcffbff5cf5a9d804f80da780b0a781e5121
neilm <neil.morrison89@gmail.com>
2020-04-23 19:36:39 +0100

    Added video activity to app which lets user pick video quality and frame rate of the stream from Server

---
+++ Android_Code/app/src/main/java/Code/Methods/VideoQuality.java
@@ -0,0 +1,23 @@
+package Code.Methods;
+
+public class VideoQuality {
+
+    private String Video;
+    private String Frames;
+
+    public String getVideo() {
+        return Video;
+    }
+
+    public void setVideo(String video) {
+        Video = video;
+    }
+
+    public String getFrames() {
+        return Frames;
+    }
+
+    public void setFrames(String frames) {
+        Frames = frames;
+    }
+}
